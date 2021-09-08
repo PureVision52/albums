e29d270ae953dc97d3f8652610670d42ed6aba63
PureVision52 <89786713+PureVision52@users.noreply.github.com>
2021-09-08 14:19:27 +0300

    Add files via upload

---
+++ precache-manifest.9a2feb615bfcf9adf03547bff97162c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa7a83d308a9db81b854a8339426c6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a09a5b0bf415b80a5a3b",
+    "url": "/static/css/main.7fdaa597.chunk.css"
+  },
+  {
+    "revision": "192783ba7c43cfd16ffd",
+    "url": "/static/js/2.c441f153.chunk.js"
+  },
+  {
+    "revision": "a09a5b0bf415b80a5a3b",
+    "url": "/static/js/main.7d4e607e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
